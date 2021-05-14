947cf694f86d2cd9d3a897e8cb7201dacbb0abae
AravinthSelvaraj <aravinthjuly28th@gmail.com>
2021-05-15 00:33:03 +0530

    0.1.2 - user-management 0.1.2 - hello world code cleanup
    Remove as much code possibly unnecessary for simple hello world rendering

--- user-management/src/App.test.tsx
+++ user-management/src/App.test.tsx
@@ -1,9 +1,12 @@
+/* eslint-env jest */
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world div', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByText(/hello world/i);
+  expect(divElement).toBeInTheDocument();
 });
--- user-management/versions.md
+++ user-management/versions.md
@@ -1,2 +1,3 @@
 0.1.0 - create-react-app typescript template
 0.1.1 - dependency clean up
+0.1.2 - hello world code cleanup
